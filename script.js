e4971ae7b991f39bd0bd25786141a695cda0a4ba
Arash <arash_raji@yahoo.com>
2021-01-28 13:47:26 +0100

    use dash in property name

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-let car = {
-    color: 'red',
-    year: 2000,
-    company: 'benz',
-    start: function() {
-        console.log('started');
-    }
+let obj = {
+    'my-property': 1000
 }
-console.log(car);
-console.log(car.company);
-car.weigth = 500;
-console.log(car);
