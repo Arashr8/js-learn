216152d077661f817ba14451b57bbc02d85ef1be
Arash <arash_raji@yahoo.com>
2021-01-28 13:37:03 +0100

    new way to declar property

--- script.js
+++ script.js
@@ -1,16 +1,5 @@
-let car = new Object()
-car.speed = 100;
-car.name = 'benz';
-car.year = 2000;
-car.start = function() {
-    console.log('started');
-}
-car.break = function() {
-    console.log('slow');
-}
-console.log('speed is ' + car.speed);
-console.log('name is ' + car.name);
-console.log('year is ' + car.year);
+let obj = new Object()
+obj['property1'] = 100
+console.log(obj.property1);
 
-car.start()
-car.break()
+obj['method']()
