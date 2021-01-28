81b0d2e1bdc1593d472391428267ba569f43eec3
Arash <arash_raji@yahoo.com>
2021-01-28 12:18:03 +0100

    first code project has been added

---
+++ script-old.js
@@ -0,0 +1,223 @@
+/* let age = 40;
+let size = 32;
+let total = age * size;
+ */
+
+/* let age, size, total;
+age = 40;
+size = 28;
+total = age * size */
+
+/* console.log(total) */
+/* let firstName = 'Ali';
+let lastName = 'Javadi';
+
+console.log(firstName)
+console.log(lastName) */
+/* let age = 22;
+console.log(age)
+
+let name = 'Saeed';
+console.log(name)
+
+let a = false
+console.log(a)
+
+let b = null
+console.log(b)
+
+let c;
+console.log(c)
+ */
+
+/* let a = 30,
+    b = 25,
+    c = 20,
+    answer;
+answer = a + b + c;
+answer = answer + a
+answer += a
+    answer++
+answer += 1 */
+
+
+/* let a = 10;
+let b = 15;
+let answer = ++a * b;
+/* b -= 2 */
+/* b--;
+b--; 
+ */
+/* let a = 2;
+let b = 3;
+b = b ** 2;
+let answer = a + b;
+answer *= 2 */
+/* answer = answer * 2 */
+
+/* variable 
+function
+object 
+property 
+method  */
+
+
+
+/* console.log(Number.isInteger(-10)) */
+/* console.log(Number.isFinite(20 / 0)) */
+/* console.log(Number.isFinite(Infinity)) */
+/* console.log(2 * 'hello')
+console.log(3 - 'hasan') */
+/* console.log(Number.isNaN(10 * 'hassan'))
+console.log(Number.isNaN(10 * 5))
+console.log(Number.isNaN(Infinity))
+console.log(Number.isNaN(NaN))
+console.log(Number.isFinite(NaN)) */
+
+/* let d = 40345 */
+/* console.log(d.toExponential()) */
+
+/* console.log(d.toFixed(3)) */
+/* console.log(d.toPrecision(2)) */
+/* 
+console.log(5000..toExponential()) */
+/* console.log(5000 .toExponential()) */
+/* console.log(5000.0.toExponential()) */
+/* console.log((5000).toExponential()) */
+
+/* let firstName = 'Reza'
+console.log(firstName)
+let lastName = "Rezaie"
+console.log(lastName)
+let str = "it's a car"
+console.log(str) */
+
+/* let str = 'my name is \\arash\\ and it\'s my car';
+
+str = 'my name is\n \\arash\\ and it\'s my car';
+console.log(str) */
+
+/* let car = 'BMW';
+let num = 30;
+let str = `my car is ${car}`;
+let str2 = `this ${car} number is ${num}`
+console.log(str2) */
+/* let str = 'Hello world salam tehran javascript'; */
+/* console.log(str.length)
+console.log(str.toUpperCase())
+console.log(str.toLowerCase()) */
+/* console.log(str.charAt(6)) */
+/* console.log(str.indexOf('world')) */
+/* console.log(str.indexOf('l', 4)) */
+
+/* console.log(str.lastIndexOf('l')) */
+/* console.log(str.includes('hi')) */
+/* console.log(str.startsWith('Hello')) */
+
+/* console.log(str.endsWith('javascript')) */
+
+/* console.log(str.substring(5, 10)) */
+/* let str2 = 'javascript'
+let str3 = 'hello ' */
+
+/* 
+console.log(str2.repeat(4))
+console.log(str3.repeat(5)) */
+
+/* console.log(str.substring(5, 10).repeat(3)) */
+/* console.log(str3.concat('world ', 'man arash hastam')) */
+/* console.log(str3 + 'world' + 'man arash hastam') */
+/* console.log('3' * 4)
+console.log('hello' * 4)
+console.log('5.4' * 4)
+console.log('' * 2) */
+/* console.log('7' + 2) */
+/* let a = '200'
+let c = 'hello'
+let b = Number(a)
+console.log(typeof b);
+console.log(a);
+console.log(b);
+console.log(Number(c)); */
+
+/* console.log(String(100));
+console.log(String(100.12));
+console.log(String(NaN)); */
+
+/* console.log(100..toString());
+console.log(NaN.toString()); */
+/* 
+console.log(parseInt('200abc'));
+console.log(parseInt('   '));
+console.log(parseInt('-4')); */
+
+/* console.log(parseFloat('200abc'));
+console.log(parseFloat('2.5ab'));
+console.log(parseInt('2.6ab'));
+console.log(parseInt(undefined)); */
+
+/* console.log(5 * undefined); */
+/* console.log('5' + undefined); */
+/* console.log(Number(null));
+console.log(parseInt(null));
+console.log(null + 10);
+console.log(null + '10'); */
+/* console.log(true + 10);
+console.log(false + 10);
+console.log(false + '10');
+console.log(true + '10'); */
+
+/* console.log(Boolean(0));
+console.log(Boolean(''));
+console.log(Boolean('   '));
+console.log(Boolean(NaN));
+console.log(Boolean(undefined));
+console.log(Boolean(null));
+console.log(Boolean('0'));
+console.log(Boolean(10.2)); */
+
+
+/* let total; */
+/* total = true */
+/* total = !true */
+/* total = !false */
+/* total = !0 */
+/* 
+total = !'arash' */
+/* total = !null */
+
+/* total = true && false */
+/* total = false && true */
+/* total = true && true */
+/* total = 'arash' && true */
+/* total = true && 'arash' */
+/* total = 0 && false */
+/* total = !'0' && true */
+/* total = true && true && false && true */
+/* 
+total = true || false */
+/* total = false || false */
+/* total = false || 'arash' */
+/* total = false || true || false || false
+console.log(total); */
+
+
+/* let a = 10;
+let b = 20;
+let c = '10';
+let d = 'javascript';
+let answer; */
+/* answer = a == b */
+/* answer = a == c */
+/* answer = a == d */
+/* answer = b == 20 */
+/* answer = 50 == 60 */
+/* answer = 0 == '0'
+
+console.log(answer); */
+
+let a = 40
+let b = 'hamid'
+let ans = a + b
+
+console.log(ans);
---
+++ script.js
@@ -0,0 +1,941 @@
+/* let a = 20
+let b = 'hamid'
+let ans = a + b
+console.log(ans);
+ */
+/* let a = '1000'
+let b = parseInt(a)
+console.log(b); */
+
+/* let a = 100
+let b = 40
+let c = a + b
+let e = c * 10
+console.log(e); */
+
+/* let s = 0 && 'hassan' && true && null && 1 && '1'
+console.log(s);
+
+let b = false || 5 || '0' || null || undefined
+console.log(b); */
+
+/* let ans = ' ' == 0 */
+/* let ans = ' ' == '0' */
+/* let ans = false == '0' */
+
+/* let ans = '1' == true */
+/* let ans = '2' == true */
+/* let ans = 'true' == true */
+/* let ans = null == undefined
+console.log(ans); */
+
+
+/* let ans = ' ' === 0 */
+/*  let ans = ' ' === '0' */
+/* let ans = false === '0'  */
+/* let ans = 1 === '1' */
+/* let ans = '1' === '1' */
+/* let ans = null === undefined */
+/* let ans = NaN === NaN
+console.log(ans); */
+
+
+/* let ans = 10 != '10'
+let ans = 10 == '10'
+let ans = 10 === '10'
+let ans = 10 !== '10' */
+/* let ans = 10 > 5
+ans = 10 < 5
+ans = 10 >= 5
+ans = 10 >= 10
+ans = 10 >= 10
+ans = 10 <= '10'
+ans = 3 <= 5
+ans = 'ali' < 'hassan'
+ans = 'ali' < 'Hassan'
+ans = 'ali' < 'aslan'
+console.log(ans); */
+
+/* sathe tahsilat 
+age 
+location 
+ 
+sharte aval > balaye lisans 
+sen >= 25 
+shahr = tehran
+
+1 = zire diplom 
+2= diplom 
+3= foghe diplom 
+4= lisans
+5= foghe
+6= doktora */
+/* let education = 5
+let age = 26
+let city = 'tehran'
+
+
+
+let condition1 = education >= 4
+let condition2 = age > 25
+let condition3 = city = 'tehran'
+let ans = condition1 && condition2 && condition3
+console.log(ans); */
+
+/* let ans = (2 + 5) * 8
+ans = (true || true) && false
+console.log(ans); */
+/* 
+if(condition){
+    statement 
+} */
+
+/* let nomre = 12
+if (nomre > 10) {
+    console.log(nomre);
+} */
+
+/* let nomre = 20
+if (nomre < 10) {
+    alert('mardood')
+}
+if (nomre >= 10) {
+    alert('ghabool')
+    if (nomre === 20) {
+        alert('behtarin nomre')
+    }
+} */
+
+/* if (condition) {
+    statement
+} else {
+    statement
+} */
+
+/* let nomre = 21
+if (nomre >= 0 && nomre <= 20) {
+    console.log('nomre ok');
+} else {
+    console.log('namotabar');
+} */
+
+/* let time = 19
+if (time >= 6 && time <= 18) {
+    console.log('goodmorning');
+} else {
+    console.log('good night');
+} */
+
+/* let person1 = 'hassan khosrojerdi'
+let person2 = 'ali karimi'
+if (person1 === 'hassan khosrojerdi' || person2 === 'ali rahmani') {
+    console.log('welcome');
+} else {
+    console.log('stop');
+} */
+
+/* let ans = condition ? expertion1 : expertion2 */
+/* let time = 12
+
+(time >= 6 && time <= 18) ? console.log('goodmorning') : console.log('good night');
+ */
+
+/* let mySalary = 1000;
+let yourSalary = 500;
+
+(mySalary > yourSalary) ? console.log('my salary is better'): console.log('your salary is better'); */
+
+/* if (condition1) {
+    statement1
+} else if (condition2) {
+    statement2
+} else if (condition3) {
+    statement3
+} else {
+    statment4
+} */
+
+/* let nomre = -100
+if (nomre >= 15 && nomre <= 20) {
+    console.log('best');
+} else if (nomre >= 10 && nomre < 15) {
+    console.log('khob');
+} else if (nomre >= 5 && nomre < 10) {
+    console.log('zaief');
+} else if (nomre >= 0 && nomre < 5) {
+    console.log('kheyli zaief');
+} else {
+    console.log('namotabar');
+} */
+
+/* switch (expression) {
+    case value1:
+        statement1
+        break;
+    case value2:
+        statement2
+        break;
+    case value3:
+        statement3
+        break;
+    default:
+        statement4
+} */
+/* 
+let num = 1000
+switch (true) {
+    case (num <= 20 && num >= 15):
+        console.log('A')
+        break;
+    case (num < 15 && num >= 10):
+        console.log('B')
+        break;
+    case (num < 10 && num >= 0):
+        console.log('C')
+        break;
+    default:
+        console.log('F');
+} */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* let array1 = new Array(10, 50, 'reza', true) */
+/* console.log(typeof array1); */
+/* console.log(array1); */
+
+/* let array2 = [10, 50, 'reza', true]
+console.log(array2); */
+/*  
+console.log(array2[0]);
+array2[0] = 5
+console.log(array2);
+array2[3] = false
+console.log(array2); */
+
+/* let array3 = [20, [5, 6, 7], 'salam', true]
+console.log(array3);
+console.log(array3[1][1]);
+console.log(array3[1][2]); */
+/* console.log(array2.length); */
+/* array2.length = 2 */
+
+/* array2.length = 6 */
+/* delete array2[1]
+console.log(array2.length);
+
+console.log(array2); */
+
+/* let num = 5
+if (num % 2 == 0)
+    console.log(num + ' zoj ast');
+else
+    console.log(num + ' fard ast'); */
+
+/* let array1 = [20, 30, 'ali']
+let array2 = [20, 30, 'ali'] */
+/* console.log(array1 == array2);
+console.log(array1[1] == array2[1]); */
+
+/* let array3 = array1
+console.log(array1 == array3);
+
+array1[0] = 50
+console.log(array3); */
+
+/* let car = ['bmw', 'benz', 'audi']
+console.log(car);
+car[0] = 'praid'
+console.log(car); */
+
+
+/* const car = ['bmw', 'benz', 'audi']
+console.log(car);
+car[0] = 'praid'
+console.log(car);
+car = ['peguet', 'volvo', 'motor']
+console.log(car); */
+
+/* let [fname, lastname, age] = ['mohammad', 'hosseini', 28]
+console.log(fname, lastname, age);
+ */
+
+/* let car = ['bmw', 'benz', 'audi', ] */
+/* let a = car.pop() */
+/* console.log(a);
+console.log(car); */
+
+/* let b = car.push('renualt')
+console.log(b);
+console.log(car); */
+
+/* let c = car.shift()
+console.log(c);
+console.log(car); */
+
+
+/* let d = car.unshift('persia')
+console.log(d);
+console.log(car); */
+
+/* car = car.concat(['persia', 'megan'])
+console.log(car);
+ */
+/* let car2 = ['persia', 'megan'] */
+/* let carTotal = [car, car2]
+console.log(carTotal); */
+
+/* let carTotal = [...car, ...car2]
+console.log(carTotal); */
+/* let carTotal = [...car, ...car2]
+console.log(carTotal.join('_')); */
+
+
+
+
+
+
+
+/* let car = ['bmw', 'benz', 'audi', 'volvo'] */
+/* let a = car.slice(1, 3) */
+/* console.log(a);
+console.log(car); */
+/* a = car.splice(1, 2) */
+/* let a = car.splice(1, 2, 'pride', 'persia')
+console.log(a);
+console.log(car); */
+
+/* let a = car.reverse() */
+/* let a = car.sort() */
+/* let a = car.indexOf('bmw')
+a = car.indexOf('audi') */
+/* let a = car.includes('volvo')
+console.log(a); */
+/* console.log(car); */
+
+/* 
+let n = 0
+while (n < 10) {
+    console.log('salam');
+    n++
+} */
+
+/* let n = 0
+while (n <= 5) {
+    console.log('salam');
+    n++
+} */
+
+/* let n = 1
+while (n <= 10) {
+    console.log(n);
+    n++
+} */
+
+/* let n = 2
+while (n >= 0) {
+    console.log(n);
+    n--
+
+} */
+
+/* n=2 
+2
+1
+
+
+n=1
+1
+0
+
+n=0
+0
+-1 */
+
+/* let a = 1
+while (a <= 100) {
+    console.log(a);
+    a++
+} */
+
+/* let b = 500
+while (b >= 1) {
+    console.log(b);
+    b--
+} */
+
+/* let c = 380
+while (c <= 420) {
+    console.log(c);
+    c++
+} */
+
+/* let d = 833
+while (d >= 501) {
+    console.log(d);
+    d--
+} */
+
+/* let num = 1
+let sum = 0
+while (num <= 50) { */
+/*   sum = sum + num */
+/*  sum+=num  
+  num++
+
+}
+alert(sum)
+ */
+
+/* 1
+0
+sum=1
+sum=3
+sum=6
+sum=10 */
+
+
+
+/* let n = 6
+do {
+    console.log(n);
+    n++
+
+}
+while (n < 5) */
+
+
+/* for (let n = 0; n < 5; n++) {
+    console.log(n);
+}
+ */
+/* for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+        console.log(j);
+    }
+
+} */
+
+/* let numbers = [10, 3, 5] */
+
+/* let numbers = [4, 3, 7, 10, 5, 11, 2] */
+
+/* for (let i = 0; i < numbers.length; i += 2) {
+    numbers[i] *= 2
+}
+console.log(numbers); */
+
+
+
+/* 
+let cars = ['benz', 'bmw', 'volvo']
+for (let car of cars) {
+    console.log(car);
+} */
+
+
+
+/* let numbers = [4, 3, 7, 10, 5, 11, 2]
+for (let number of numbers) {
+
+    if (number % 2 == 0) {
+        console.log(number + ' zoj ast ');
+
+    } else {
+        console.log(number + ' fard ast ');
+    }
+
+} */
+
+
+/* for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 == 0){
+        console.log(numbers[i] + ' zoj ast');
+    }
+    else{
+        console.log(numbers[i] + ' fard ast');
+    }
+     
+} */
+
+/* for (let i = 0; i < 10; i++) {
+    if (i == 4) {
+        continue
+    }
+    console.log(i);
+
+} */
+
+/* let mySet = new Set()
+    /* console.log(mySet); */
+
+/* mySet.add(5).add('hassan').add('ali')
+console.log(mySet);  */
+
+/* let mySet = new Set([1, 2, 3, 3, 4, 4, 5])
+console.log(mySet); */
+
+
+/* let mySet = new Set(['ali', 'hassan', 'amin'])
+mySet = new Set(['ali', 'hassan', 'amin', 228, '228'])
+console.log(mySet);
+console.log(mySet.size);
+console.log(mySet.has('nima'));
+
+let a = mySet.delete('hassan')
+console.log(mySet);
+mySet.clear()
+console.log(mySet); */
+
+/* let myMap = new Map()
+myMap.set(1, 'ali').set(2, 'hassan').set(3, 5)
+console.log(myMap);
+myMap.set('name', 'nima')
+console.log(myMap); */
+
+
+/* let person = new Map([
+    ['firstr name', 'hassan'],
+    ['last name', 'khosro'],
+    ['age', 30]
+
+])
+console.log(person);
+console.log(person.size);
+console.log(person.get('last name'));
+console.log(person.delete('age'));
+console.log(person);
+person.clear();
+console.log(person); */
+
+
+/* let mySet = new Set(['hassan', 'ali', 'karim'])
+console.log(mySet); */
+/* let myArray = [...mySet]
+console.log(myArray); */
+
+/* let myArray = Array.from(mySet)
+console.log(myArray); */
+
+
+
+/* let person = new Map([
+    ['firstr name', 'hassan'],
+    ['last name', 'khosro'],
+    ['age', 30]
+
+
+])
+let myArray = Array.from(person)
+console.log(myArray); */
+
+/* let b = [5, 10, 12, 20, 45, 33, 10, 5, 20]
+let mySet = new Set(b)
+b = Array.from(mySet)
+console.log(b); */
+/* 
+let mySet = new Set(['hassan', 'ali', 'karim'])
+
+for (let name of mySet){
+    console.log(name);
+}
+   */
+
+/* let person = new Map([
+    ['firstr name', 'hassan'],
+    ['last name', 'khosro'],
+    ['age', 30]
+
+
+])
+ */
+/* for (let key of person.keys()) {
+    console.log(key);
+}
+
+for (let value of person.values()) {
+    console.log(value);
+} */
+/* for (let [key, value] of person.entries()) {
+    console.log(key + '=' + value);
+} */
+
+/* function showInfo() {
+    console.log('firstName : hassan');
+    console.log('lastName : hadian');
+    console.log('age : 30');
+}
+showInfo();
+showInfo(); */
+
+/* const showInfo = function() {
+    console.log('firstName : hassan');
+    console.log('lastName : hadian');
+    console.log('age : 30');
+}
+
+showInfo()
+
+let a = showInfo
+a() */
+/* 
+function showInfo(firstName, lastName, age) {
+
+    console.log('Namn : ' + firstName);
+    console.log('efternamn : ' + lastName);
+    console.log('gammal : ' + age);
+
+}
+showInfo('ali', 'karimi', 10)
+showInfo('hassan', 'khosro', 30) */
+
+
+/* function add() {
+    console.log(3 + 5);
+}
+add() */
+
+/* function add(a, b) {
+    console.log(a + b);
+
+}
+add(1000, 20000) */
+
+/* function showMsg(firstName, lastName) {
+    alert('salam ' + firstName + ' ' + lastName)
+}
+showMsg('hassan', 'khosro') */
+
+/* function square(c) {
+    console.log(c * c);
+}
+square(20) */
+
+/* function avg(a, b, c, d) {
+    let total = a + b + c + d
+    let average = total / 4
+    return average; */
+/* console.log('salam'); ejra nemishavad */
+/* }
+let st1 = avg(10, 20, 5, 1)
+let st2 = avg(15, 20, 17, 16)
+console.log(st1, st2); */
+/* 
+function myFunc() {
+    console.log(arguments.length);
+
+}
+myFunc('hassan', 'reza', 'hamid') */
+
+/* function avg() {
+    let total = 0;
+    let average;
+    for (let number of arguments) {
+        total += number
+    }
+    average = total / arguments.length
+    return average
+
+
+}
+
+console.log(avg(2, 6, 12, 10, 20)); */
+
+/* function avg(...numbers) {
+    let total = 0;
+    let average;
+    for (let number of numbers) {
+        total += number
+    }
+    average = total / numbers.length
+    return average
+
+
+} */
+
+/* 
+function power(base, count=2 ) {
+    let result = 1
+    for (let i = 0; i < count; i++) {
+        result = result*base
+    }
+    return result
+}
+
+power(3, 3)
+power(3)
+
+
+
+base      count        result     i 
+3          3             1        0
+                         3        1
+                         9        2
+                         27       3
+ */
+
+
+
+
+
+/* power(2, 3)
+
+
+
+base      count        result       i 
+ */
+/* 2           3            1          0
+                         2          1
+                    
+                         4          2
+
+                         8          3
+ */
+
+
+
+
+
+
+
+/* yek function ke chand addad begirad ( namahdod)
+va motenaseb ba operatori ke ma barash moshakhas kardim amaliat roanjam Bede 
+
+calc ('+',2,20,3,50)
+
+calc ('*',2,20,3,50)
+ */
+
+/* function calc(operation, ...numbers) {
+    let result;
+    if (operation === '+') {
+        result = 0;
+        for (let number of numbers) {
+            result = result + number
+        }
+    } else if (operation === '*') {
+        result = 1; 
+        for (let number of numbers) {
+            result = result * number
+        }
+    }
+    return result
+}
+console.log(calc('+', 5, 10, 15));
+console.log(calc('*', 3, 5)) */
+;
+
+/* 
+function myFunc(x) {
+    return x * x;
+}
+console.log(myFunc(5)); */
+
+/* const myFunc = function(x){
+    return x*x ;
+} */
+
+/* arrow function */
+
+/* const myFunc = x => x * x
+console.log(myFunc(5)); */
+
+/* const myFunc = (x, y) => x + y
+console.log(myFunc(5, 10)); */
+
+/* const myFunc = () => 'hello'
+console.log(myFunc()); */
+/* function myFunc() {
+    return 'hello'
+}
+console.log(myFunc()); */
+
+/* const myFunc = (x) => {
+    let a = x + x
+    let b = a - 2
+    return b
+}
+console.log(myFunc(5)); */
+
+
+
+/* var a = 20
+
+function myFunc() {
+    var b = 50 
+          console.log(a);
+         console.log(b); 
+     if (true) {
+        var c = 60 
+             console.log(a);
+            console.log(b);
+            console.log(c); 
+    } 
+     console.log(a);
+     console.log(b);
+     console.log(c); 
+ }
+myFunc();
+console.log(a);
+console.log(b); 
+ console.log(c);   */
+
+
+
+
+
+
+/* let a = 20
+
+function myFunc() {
+    let b = 50 */
+/*  console.log(a);
+ console.log(b); */
+/*  if (true) {
+     let c = 60 */
+/*  console.log(a);
+ console.log(b);
+ console.log(c); */
+/*  } */
+/* console.log(a);
+console.log(b);
+console.log(c); */
+/* }
+myFunc();
+console.log(a);
+console.log(b);
+console.log(c); */
+
+/* 
+a = 30
+console.log(a); */
+
+/* function myFunc() {
+    b = 30
+}
+myFunc()
+console.log(b); */
+
+
+/* let a = 30
+a = 50 */
+
+/* let a = 10
+
+function myFunc() {
+    let a = 50
+    console.log(a);
+    a = 30
+    console.log(a);
+}
+myFunc()
+console.log(a); */
+
+/* function outer() {
+    function inner() {
+        console.log('this is inner');
+    }
+    inner()
+    console.log('this is outer');
+}
+outer() */
+
+/* function myFunc() {
+    var a;
+    console.log(a);
+    var a = 50
+}
+myFunc() */
+
+/* myFunc()
+
+function myFunc() {
+    console.log('salam');
+} */
+
+/* myFunc()
+let myFunc = function() {
+    console.log('salam');
+} */
+
+/* function myFunc(x) {
+    console.log(x);
+}
+myFunc('hassan') */
+
+/* function myFunc(callback) {
+    callback()
+}
+ */
+/* function test() {
+    console.log('salam');
+}
+myFunc(test)
+ */
+
+/* const test = function() {
+    console.log('salam');
+}
+myFunc(test) */
+
+/* myFunc(function() {
+    console.log('salam');
+}) */
+
+/* myFunc(() => console.log('salam')); */
+
+/* myFunc(10) */
+
+/* let numbers = [10, 15, 20, 5, 2] */
+/* let result = numbers.sort()
+console.log(result);  */
+/* function compare(value1, value2) {
+    if (value1 < value2)
+        return -1
+    else if (value1 > value2)
+        return 1
+    else
+        return 0
+}
+let result = numbers.sort(compare)
+console.log(result); */
+
+/* function compare(value1, value2) {
+    return value1 - value2
+} */
+
+
+/* let result = numbers.sort((value1, value2) => value1 - value2)
+console.log(result); */
+
+/* function show(value) {
+    console.log(value.toUpperCase());
+}
+let cars = ['benz', 'bmw', 'volvo']
+cars.forEach(show) */
+
+/* let cars = ['benz', 'bmw', 'volvo']
+cars.forEach((value, index) => console.log(value + ' ' + index)) */
+
+let arr1 = [20, 15, 'hamid', 'kaveh', 50, 60, 33, 45, 1]
+let result = arr1.filter(value => value % 2 === 0)
+console.log(result);
+console.log(arr1);
