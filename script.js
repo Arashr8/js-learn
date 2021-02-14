2862407bd22ba9fc942217779b135e7b54edbe68
Arash <arash_raji@yahoo.com>
2021-02-14 11:59:02 +0100

    name space

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-/* console.log(Math);
-
-console.log(Math.PI);
-console.log(Math.E);
-console.log(Math.LN2); */
-
-console.log(Math.round(4.8));
-console.log(Math.pow(3, 2));
-console.log(Math.sqrt(64));
-console.log(Math.abs(-5));
-console.log(Math.ceil(4.4));
-console.log(Math.floor(4.7));
-console.log(Math.min(4, 8, 10, -3));
-console.log(Math.max(4, 8, 10, -3));
-console.log(Math.floor(Math.random() * 101));
+console.log(math.log(4))
