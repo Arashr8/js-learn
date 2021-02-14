79f977f421a4ad6c8e2996709cdc9ad1f288ef6d
Arash <arash_raji@yahoo.com>
2021-02-14 11:42:01 +0100

    object insatceof

--- script.js
+++ script.js
@@ -6,3 +6,8 @@ let car1 = new Car('red', 200)
 let car2 = new Car('blue', 300)
 console.log(car1);
 console.log(car2);
+console.log(typeof car1);
+
+console.log(car1 instanceof Car);
+console.log(car1 instanceof Object);
+console.log(car1 instanceof Array);
