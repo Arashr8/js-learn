80569ae4eabaca52ee98a250bd405d49dba8af37
Arash <arash_raji@yahoo.com>
2021-02-20 09:13:33 +0100

    example elemnet.length

--- script.js
+++ script.js
@@ -1,9 +1,2 @@
-let elem = document.getElementsByTagName('p')
-console.log(elem);
-console.log(elem.length);
-
-if (elem.length <= 4) {
-    console.log('it is four');
-} else {
-    console.log('it is not four');
-}
+let p = document.getElementsByTagName('p')
+console.log('there are ' + p.length + ' in this page');
