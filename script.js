d0420d751f7557b1df542652839b9887b3fb9a26
Arash <arash_raji@yahoo.com>
2021-02-20 10:03:23 +0100

    childnodes

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-console.log(document.body);
-console.log(document.head);
-console.log(document.forms);
-console.log(document.images);
-console.log(document.links);
-
-let d = document.body.querySelector('.p1')
-console.log(d);
+let main = document.getElementById('main')
+console.log(main.childNodes);
+console.log(main.children);
