2533dea8d10fa6515a3666a3d92649668943f506
Arash <arash_raji@yahoo.com>
2021-02-21 09:51:16 +0100

    next and previous sibling

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-let main = document.getElementById('main')
-console.log(main.childNodes);
-console.log(main.children);
-console.log(main.firstChild);
-console.log(main.lastChild);
-console.log(main.firstElementChild);
-console.log(main.lastElementChild);
+let heading = document.getElementById('heading')
+console.log(heading.nextSibling);
+console.log(heading.nextElementSibling.nodeName);
+console.log(heading.previousSibling);
+console.log(heading.previousElementSibling);
