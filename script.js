d35102f72dac9137bc9f87559583e6e7c02754e0
Arash <arash_raji@yahoo.com>
2021-02-21 09:41:08 +0100

    children

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 let main = document.getElementById('main')
 console.log(main.childNodes);
 console.log(main.children);
+console.log(main.firstChild);
+console.log(main.lastChild);
+console.log(main.firstElementChild);
+console.log(main.lastElementChild);
