1bdd5a04420a7f882050bd9b68f3bab5b2ba7f12
Arash <arash_raji@yahoo.com>
2021-03-13 12:44:48 +0100

    insertBefore

--- script.js
+++ script.js
@@ -3,22 +3,7 @@ function createElement(tag, text) {
     element.textContent = text
     return element
 }
-/* let main = document.getElementById('main')
-let h3 = createElement('h3', 'hello')
-let h4 = createElement('h4', 'salam h4')
-main.appendChild(h3)
-main.appendChild(h4) */
-
 let main = document.getElementById('main')
-let heading = createElement('h1', 'list of fruits')
-let ul = createElement('ul', '')
-let li = createElement('li', 'apple')
-let li2 = createElement('li', 'orange')
-let li3 = createElement('li', 'banana')
-let li4 = createElement('li', 'cherry')
-ul.appendChild(li)
-ul.appendChild(li2)
-ul.appendChild(li3)
-ul.appendChild(li4)
-main.appendChild(heading)
-main.appendChild(ul)
+let ul = document.querySelector('ul')
+let p = createElement('p', 'this is p tag')
+main.insertBefore(p, ul)
