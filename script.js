3ce0d215719623d0b4da39bf6871d43913a68587
Arash <arash_raji@yahoo.com>
2021-03-14 10:44:21 +0100

    cloneNode

--- script.js
+++ script.js
@@ -5,15 +5,7 @@ function createElement(tag, text) {
 }
 
 let ul = document.querySelector('ul')
-let second = ul.firstElementChild.nextElementSibling
-    /* console.log(second); */
-
-let removed = ul.removeChild(second)
-ul.appendChild(removed)
-
-let newItem = createElement('li', 'lemon')
-let oldItem = removed.previousElementSibling
-console.log(removed);
-console.log(newItem);
-console.log(oldItem);
-let replaced = ul.replaceChild(newItem, oldItem)
+    /* ul.replaceChild(ul.firstElementChild, ul.lastElementChild)
+     */
+let copyNode = ul.firstElementChild.cloneNode(true)
+ul.appendChild(copyNode)
