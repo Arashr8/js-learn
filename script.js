88acc2fcf9fe2d5295b45551d0cef977ad4f94b2
Arash <arash_raji@yahoo.com>
2021-03-15 18:56:51 +0100

    appendChild on click

--- script.js
+++ script.js
@@ -3,4 +3,9 @@ btn.addEventListener('click', handler)
 
 function handler() {
     btn.textContent = 'click2'
+    let p = document.createElement('p')
+    let div = document.getElementById('main')
+
+    p.textContent = 'salam'
+    div.appendChild(p)
 }
