e51ce9c8b809bea6c698aa13737967412edbde1d
Arash <arash_raji@yahoo.com>
2021-03-15 18:40:25 +0100

    change textContent

--- script.js
+++ script.js
@@ -1,8 +1,2 @@
-let ul = document.querySelector('ul')
-    /* console.log(ul.style.color);
-    console.log(ul.style.border); */
-
-let styles = getComputedStyle(ul)
-console.log(styles.getPropertyValue('color'));
-console.log(styles.getPropertyValue('font-size'));
-console.log(styles.getPropertyValue('width'));
+let text = document.querySelector('p')
+text.textContent = 'hello'
