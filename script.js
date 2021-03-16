b8daf544f23f3758263a5d5765a649be3ed4ca0e
Arash <arash_raji@yahoo.com>
2021-03-16 18:17:35 +0100

    innerhtml eventlistener

--- script.js
+++ script.js
@@ -2,10 +2,6 @@ let btn = document.querySelector('button')
 btn.addEventListener('click', handler)
 
 function handler() {
-    btn.textContent = 'click2'
-    let p = document.createElement('p')
     let div = document.getElementById('main')
-
-    p.textContent = 'salam'
-    div.appendChild(p)
+    div.innerHTML += '<p>click shode </p>'
 }
