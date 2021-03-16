ec432e6ad60aecffe299c489c54abedd462c7fc9
Arash <arash_raji@yahoo.com>
2021-03-16 18:21:18 +0100

    onclick

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 let btn = document.querySelector('button')
-btn.addEventListener('click', handler)
 
-function handler() {
-    let div = document.getElementById('main')
-    div.innerHTML += '<p>click shode </p>'
+btn.onclick = function() {
+    alert('salam')
 }
