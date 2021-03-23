2f129410e673c464475f4418953cd740abf15144
Arash <arash_raji@yahoo.com>
2021-03-23 09:52:24 +0100

    mouse evenrt

--- script.js
+++ script.js
@@ -1,18 +1,12 @@
-const main = document.getElementById('main');
-main.addEventListener('click', handler)
+const outer = document.getElementById('outer');
+/* outer.addEventListener('click', handler) */
+/* outer.addEventListener('mouseover', handler) */
+/* outer.addEventListener('mouseout', handler) */
+/* outer.addEventListener('mouseenter', handler)
+ */
+/* outer.addEventListener('mouseleave', handler)*/
+outer.addEventListener('mousemove', handler)
 
 function handler(event) {
     console.log(event.type);
-    console.log(event.target.nodeName);
-    /*  console.log(event.clientX);
-     console.log(event.clientY); */
-    /* console.log(event.offsetX);
-    console.log(event.offsetY); */
-    /* console.log(event.pageX);
-    console.log(event.pageY); */
-    /* console.log(event.screenX);
-    console.log(event.screenY); */
-    console.log(event.altKey);
-    console.log(event.ctrlKey);
-    console.log(event.shiftKey);
 }
