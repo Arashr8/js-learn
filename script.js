dc22c3e10044e2372ed43620734863edee533018
Arash <arash_raji@yahoo.com>
2021-03-25 11:56:00 +0100

    copy text example

--- script.js
+++ script.js
@@ -3,12 +3,8 @@ const input = document.querySelector('input')
 input.addEventListener('keydown', handler)
 
 function handler(event) {
-    /* console.log(event.altKey);
-    console.log(event.ctrlKey);
-    console.log(event.shiftKey); */
-    /*     console.log(event.repeat);
-     */
-    /*     console.log(event.code);
+    if (event.code == 'KeyC' && event.ctrlKey) {
+        alert('you have copied this text ')
+    }
 
-     */
 }
