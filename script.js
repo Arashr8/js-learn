3299370c368ac244e58b3ced1611cdcf71323233
Arash <arash_raji@yahoo.com>
2021-03-27 11:41:30 +0100

    Delegation

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-document.addEventListener('contextmenu', handler)
+const ul = document.querySelector('ul');
+
+ul.addEventListener('click', handler);
 
 function handler(event) {
-    event.preventDefault()
+    console.log(event.target.textContent);
+}
+
+const button = document.querySelector('button');
+button.addEventListener('click', addHandler);
+
+function addHandler() {
+    ul.innerHTML += '<li>onion</li>'
+    button.removeEventListener('click', addHandler);
 }
