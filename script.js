af862ec3373795c2f30e8e5899ec841e63174b0e
Arash <arash_raji@yahoo.com>
2021-04-19 13:08:24 +0200

    change component color

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-const button = document.querySelector('button')
-button.addEventListener('click', handler)
+const componentA = document.querySelector("#componenet_a")
+const componentB = document.querySelector("#componenet_b")
 
-function handler() {
-    document.body.style.background = 'red'
+const btnA = document.querySelector("#btn_a")
+const btnB = document.querySelector("#btn_b")
+
+btnA.addEventListener('click', handelColorA)
+
+function handelColorA() {
+    componentA.classList.add('pink')
+    componentB.classList.remove('pink')
+}
+
+
+btnB.addEventListener('click', handelColorB)
+
+function handelColorB() {
+    componentB.classList.add('pink')
+    componentA.classList.remove('pink')
 }
