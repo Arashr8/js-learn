6651935f99748ce80c38ce80fc73358c090c045a
Arash <arash_raji@yahoo.com>
2021-04-25 13:27:54 +0200

    duplicate input value

--- script.js
+++ script.js
@@ -1,29 +1,7 @@
-/* let myForm = document.forms[0]
-console.log(myForm); */
+const button = document.getElementById('btn')
+button.addEventListener('click', duplicate)
 
-/* let myForm = document.getElementsByTagName('form')[0]
-console.log(myForm); */
-
-
-
-let myForm = document.forms.search
-    /* console.log(myForm); */
-    /* 
-    let myInput = myForm.mytxt
-    console.log(myInput);
-
-    let myButton = myForm.btn
-    console.log(myButton); */
-
-
-/* console.log(myForm.action);
-console.log(myForm.method);
-console.log(myForm.leng); */
-
-let myInput = myForm.mytxt
-myInput.required = true
-myInput.readOnly = true
-myInput.disabled = true
-
-let myButton = myForm.btn
-myButton.disabled = true
+function duplicate() {
+    const input = document.getElementById('mytxt')
+    input.value += input.value
+}
