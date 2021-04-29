c9f379e0c8df73d9e447bd02dfe68d9de7c47e37
Arash <arash_raji@yahoo.com>
2021-04-29 15:08:06 +0200

    form change

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 const firstName = document.getElementById('firstName')
-firstName.addEventListener('focus', (event) => {
-    event.target.select()
+const check = document.getElementById('check')
+const textarea = document.getElementById('textarea')
+const select = document.getElementById('select')
+
+
+
+
+firstName.addEventListener('change', () => {
+    console.log('salam');
 })
 
-const lastName = document.getElementById('lastName')
-lastName.focus()
+
+check.addEventListener('change', () => {
+    console.log('salam');
+})
+
+textarea.addEventListener('change', () => {
+    console.log('salam');
+})
+
+select.addEventListener('change', () => {
+    const span = document.querySelector('span')
+    span.innerHTML = 'changed!!'
+})
