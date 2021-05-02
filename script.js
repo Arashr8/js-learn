5fd414adf1f52e602ddef116630c865308900356
Arash <arash_raji@yahoo.com>
2021-05-02 14:01:54 +0200

    validation form

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 const form = document.querySelector('form')
 
 form.addEventListener('submit', (event) => {
-    let input = document.querySelector('input')
-    if (input.value.length <= 0) {
-        alert('empty')
+    let userName = document.getElementById('userName')
+
+    if (userName.value.length < 5 || userName.value.length > 15) {
         event.preventDefault()
+        alert('Error')
+
+
     }
+    let password = document.getElementById('pass')
+
+    if (password.value.length < 8) {
+        event.preventDefault()
+        alert('Password most be 8 character')
+
+    }
+
+
 })
