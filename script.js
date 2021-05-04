21952db7de72db346faeee14523c723ff0059381
Arash <arash_raji@yahoo.com>
2021-05-04 13:57:15 +0200

    window variable

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
-const form2 = document.getElementById('form2');
-form2.addEventListener('submit', validateEmail);
+var a = 10
+var b = 20
+console.log(window.a);
 
-function validateEmail(event) {
-    const email = document.getElementById('email');
-    const emailPattern = /^[a-z0-9._-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
-    if (!emailPattern.test(email.value)) {
-        email.nextElementSibling.innerHTML = 'error';
-        event.preventDefault();
-    }
-}
+let c = 30
+console.log(window.c);
