49575348214e595e49f4e23e555f2622dbf46987
Arash <arash_raji@yahoo.com>
2021-05-04 13:48:27 +0200

    confirm password validation

--- script.js
+++ script.js
@@ -1,27 +1,9 @@
-const form = document.querySelector('form')
+let form = document.querySelector('form')
+form.addEventListener('submit', () => {
+    let password = document.getElementById('pass').value
+    let confirmPassword = document.getElementById('confirm_pass').value
 
-form.addEventListener('submit', (event) => {
-    let userName = document.getElementById('userName')
-
-    if (userName.value.length < 5 || userName.value.length > 15) {
-        event.preventDefault()
-        alert('Username Error')
-
-
+    if (password != confirmPassword) {
+        alert('error')
     }
-    let password = document.getElementById('pass')
-
-    if (password.value.length < 8) {
-        event.preventDefault()
-        alert('Password most be 8 character')
-
-    }
-
-    let age = document.getElementById('age')
-    if (!Number.isInteger(Number(age.value)) && (age.value < 1 || age.value > 100)) {
-        event.preventDefault()
-        alert('not valid')
-    }
-
-
 })
