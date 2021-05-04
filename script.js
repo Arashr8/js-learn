1310d8c7b3b6a91b6e4c7983b1088f58a7d305cc
Arash <arash_raji@yahoo.com>
2021-05-04 13:51:33 +0200

    email validation

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-let form = document.querySelector('form')
-form.addEventListener('submit', () => {
-    let password = document.getElementById('pass').value
-    let confirmPassword = document.getElementById('confirm_pass').value
+const form2 = document.getElementById('form2');
+form2.addEventListener('submit', validateEmail);
 
-    if (password != confirmPassword) {
-        alert('error')
+function validateEmail(event) {
+    const email = document.getElementById('email');
+    const emailPattern = /^[a-z0-9._-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
+    if (!emailPattern.test(email.value)) {
+        email.nextElementSibling.innerHTML = 'error';
+        event.preventDefault();
     }
-})
+}
