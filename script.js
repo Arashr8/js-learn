8c23dc3221a56c35f196f124065b6d568225b6b8
Arash <arash_raji@yahoo.com>
2021-05-06 13:46:01 +0200

    print

--- script.js
+++ script.js
@@ -1,26 +1,12 @@
-const alertBtn = document.getElementById('alert_btn')
-alertBtn.addEventListener('click', () => {
-    alert('salam')
+const button = document.querySelector('button');
+button.addEventListener('click', () => {
+    print()
 })
 
-
-const confirmBtn = document.getElementById('confirm')
-confirmBtn.addEventListener('click', () => {
-    let answer = confirm('aya razi hastid')
-    if (answer == true) {
-        alert('thanx')
-    } else {
-        alert('why not?')
-    }
+window.addEventListener('beforeprint', () => {
+    button.style.display = 'none'
 })
 
-
-const promptBtn = document.getElementById('prompt')
-promptBtn.addEventListener('click', () => {
-    let name = prompt('lotfan name khod ra vared konid')
-    let age = prompt('lotfan age khod ra vared konid', '50')
-
-    console.log('your name is ' + name + 'and your age is ' + age);
-
-
+window.addEventListener('afterprint', () => {
+    button.style.display = 'inline-block'
 })
