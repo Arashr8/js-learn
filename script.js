1034e9df0c63a87105de686426fc3c23c5ecd4bb
Arash <arash_raji@yahoo.com>
2021-05-08 12:23:15 +0200

    locatrion object

--- script.js
+++ script.js
@@ -1,17 +1,40 @@
-const back = document.getElementById('back');
-const forward = document.getElementById('forward');
-const go = document.getElementById('go');
+const href = document.getElementById('href');
+const google = document.getElementById('google');
+const assign = document.getElementById('assign');
+const replace = document.getElementById('replace')
+const reload = document.getElementById('reload');
+const details = document.getElementById('details');
+const p = document.querySelector('p');
 
-back.addEventListener('click', () => {
-    history.back();
+href.addEventListener('click', () => {
+    console.log(location.href);
+})
+
+google.addEventListener('click', () => {
+    location.href = 'https://google.com'
+})
+
+assigne.addEventListener('click', () => {
+    location.assign('https://google.com')
 })
 
 
-forward.addEventListener('click', () => {
-    history.forward();
+replace.addEventListener('click', () => {
+    location.replace('https://google.com')
 })
 
 
-go.addEventListener('click', () => {
-    history.go(2);
+reload.addEventListener('click', () => {
+    location.reload();
+})
+
+details.addEventListener('click', () => {
+    console.log(location.href);
+    console.log(location.hash);
+    console.log(location.host);
+    console.log(location.hostname);
+    console.log(location.pathname);
+    console.log(location.port);
+    console.log(location.protocol);
+    console.log(location.search);
 })
