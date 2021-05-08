4a943fb7c3126f7f728037f4732d3de02fdf1f28
Arash <arash_raji@yahoo.com>
2021-05-08 12:32:44 +0200

    navigator object

--- script.js
+++ script.js
@@ -1,40 +1,13 @@
-const href = document.getElementById('href');
-const google = document.getElementById('google');
-const assign = document.getElementById('assign');
-const replace = document.getElementById('replace')
-const reload = document.getElementById('reload');
+const agent = document.getElementById('agent');
 const details = document.getElementById('details');
-const p = document.querySelector('p');
 
-href.addEventListener('click', () => {
-    console.log(location.href);
-})
-
-google.addEventListener('click', () => {
-    location.href = 'https://google.com'
-})
-
-assigne.addEventListener('click', () => {
-    location.assign('https://google.com')
-})
-
-
-replace.addEventListener('click', () => {
-    location.replace('https://google.com')
-})
-
-
-reload.addEventListener('click', () => {
-    location.reload();
+agent.addEventListener('click', () => {
+    console.log(navigator.userAgent);
 })
 
 details.addEventListener('click', () => {
-    console.log(location.href);
-    console.log(location.hash);
-    console.log(location.host);
-    console.log(location.hostname);
-    console.log(location.pathname);
-    console.log(location.port);
-    console.log(location.protocol);
-    console.log(location.search);
+    console.log(navigator.cookieEnabled);
+    console.log(navigator.language);
+    console.log(navigator.onLine);
+    console.log(navigator.platform);
 })
