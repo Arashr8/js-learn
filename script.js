26a449ddfc761717ffd1af6f0db5b45844157b2c
Arash <arash_raji@yahoo.com>
2021-05-09 14:07:39 +0200

    load in page popup

--- script.js
+++ script.js
@@ -1,5 +1,1 @@
-window.addEventListener('click', () => {
-
-
-    open('http://toplearn.com', '_blank', 'width=500,height=500,left=700,top=500');
-})
+open('http://toplearn.com', '_blank', 'width=500,height=500,left=700,top=500');
