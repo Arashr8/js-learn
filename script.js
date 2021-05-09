565b9e14f0bd7384844e93b73e0937b77cbfc7f1
Arash <arash_raji@yahoo.com>
2021-05-09 13:42:36 +0200

    open and close function

--- script.js
+++ script.js
@@ -1,24 +1,10 @@
-const total = document.getElementById('total');
-const available = document.getElementById('available');
-const browser = document.getElementById('browser');
-const viewport = document.getElementById('viewport');
-
-total.addEventListener('click', () => {
-    console.log(screen.width);
-    console.log(screen.height);
+const openBtn = document.getElementById('open');
+const closeBtn = document.getElementById('close');
+let adress
+openBtn.addEventListener('click', () => {
+    adress = open('https://github.com/');
 })
 
-available.addEventListener('click', () => {
-    console.log(screen.availWidth);
-    console.log(screen.availHeight);
-})
-
-browser.addEventListener('click', () => {
-    console.log(window.outerWidth);
-    console.log(window.outerHeight);
-})
-
-viewport.addEventListener('click', () => {
-    console.log(window.innerWidth);
-    console.log(window.innerHeight);
+closeBtn.addEventListener('click', () => {
+    adress.close();
 })
