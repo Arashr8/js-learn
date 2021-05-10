ca70c92bbfe797b7c0ce4cfa61d0b91f7d96bc77
Arash <arash_raji@yahoo.com>
2021-05-10 19:57:19 +0200

    logical error

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-open('http://toplearn.com', '_blank', 'width=500,height=500,left=700,top=500');
+let username = prompt('نام کاربری خود را وارد کنید');
+if (username == "admin") {
+    console.log('نام کاربری صحیح است');
+} else {
+    console.log('نام کاربری اشتباه است');
+}
+
+function concat(str1, str2, str3 = '') {
+    let result = str1 + str2;
+    if (str3 !== '') {
+        result += str3;
+    }
+    console.log(result);
+}
+concat('salam', 1, 0);
