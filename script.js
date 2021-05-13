e1df8c61ccacb440d7cc0a1dd1cf4d1efc90784b
Arash <arash_raji@yahoo.com>
2021-05-13 20:32:25 +0200

    ES5

--- script.js
+++ script.js
@@ -1,18 +1,54 @@
-/* throw 'hassan' */
-/* 
-throw new RangeError('value is too big'); */
+/* let str = '    salam         '
+alert(str.trim()) */
 
 
-function example(str) {
-    if (typeof str != "string") {
-        throw new TypeError('Input type is wrong!');
+/* let arr = 'havij'
+console.log(Array.isArray(arr));
+
+let numbers = [2, 3, 4, 5, 10, 11]
+numbers.forEach((number) => {
+    number++
+    console.log(number);
+}) */
+
+/* let numbers = [2, 3, 4, 5, 10, 11] */
+/* let numbers2 = numbers.map((number) => {
+    return number * 2
+})
+
+console.log(numbers2); */
+
+
+/* let numbers = [2, 3, 4, 5, 10, 11]
+let numbers2 = numbers.filter((number) => {
+    return number > 5
+})
+
+console.log(numbers2); */
+
+/* let car = {
+    color: 'red',
+    model: 'bmw',
+    years: 2000,
+    get info() {
+        return this.color + ' ' + this.model + ' ' + this.years;
+    },
+    set year(value) {
+        this.years = value;
     }
-    console.log('Type is correct.');
+
 }
-try {
-    example(20)
 
-} catch (error) {
+car.year = 2010
+car.year = 2020
 
-    console.log(error.message);
+console.log(car.info); */
+
+/* let str = 'hello \
+ world';
+console.log(str); */
+
+let person = {
+    new: 'hassan',
+    name: 'ali'
 }
