5285b98db4d2be47b16ec9cf18a0ea5cbe84ea67
Arash <arash_raji@yahoo.com>
2021-05-17 18:54:38 +0200

    ES 6 , symbol and function parametr

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
-let status = false;
+const person = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 50,
+    eyeColor: "blue"
+};
 
-let myPromise = new Promise(function(myresolve, myreject) {
-    if (status === true) {
-        setTimeout(function() {
-                myresolve('i love you');
-            },
-            3000
-        )
-    } else {
-        myreject('reject')
-    }
-})
+let id = Symbol('id');
+person[id] = 140353;
+// Now Person[id] = 140353
+// but person.id is still undefined
 
-myPromise.then(
-    function(value) {
-        console.log(value);
-    },
-    function(err) {
-        console.log(err);
-    }
-)
+console.log(Symbol('id') == Symbol('id'));
+
+function myFunction(x, y = 10) {
+    // y is 10 if not passed or undefined
+    return x + y;
+}
+myFunction(5); // will return 15
+
+
+
+
+function sum(...args) {
+    let total = 0;
+    for (let arg of args)
+        total += arg;
+    return total;
+}
+
+let x = sum(4, 9, 16, 25, 29, 100, 66, 77);
