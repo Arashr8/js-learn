85534ead65fe35d6da757552605b11dfdd590b6b
Arash <arash_raji@yahoo.com>
2021-05-17 18:59:22 +0200

    ES 6 , find

--- script.js
+++ script.js
@@ -1,31 +1,6 @@
-const person = {
-    firstName: "John",
-    lastName: "Doe",
-    age: 50,
-    eyeColor: "blue"
-};
+var numbers = [4, 9, 16, 25, 29];
+var first = numbers.find(myFunction);
 
-let id = Symbol('id');
-person[id] = 140353;
-// Now Person[id] = 140353
-// but person.id is still undefined
-
-console.log(Symbol('id') == Symbol('id'));
-
-function myFunction(x, y = 10) {
-    // y is 10 if not passed or undefined
-    return x + y;
+function myFunction(value, index, array) {
+    return value > 18;
 }
-myFunction(5); // will return 15
-
-
-
-
-function sum(...args) {
-    let total = 0;
-    for (let arg of args)
-        total += arg;
-    return total;
-}
-
-let x = sum(4, 9, 16, 25, 29, 100, 66, 77);
