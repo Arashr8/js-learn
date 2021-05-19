0cd5469f539c4478c51079a3475ac343d445d146
Arash <arash_raji@yahoo.com>
2021-05-19 09:29:23 +0200

    Ecma script 2017

--- script.js
+++ script.js
@@ -1,6 +1,40 @@
-let a = 3
-console.log(a ** 2);
+/* const person = {
+    firstName: 'Danial',
+    lastName: 'Hosseini',
+    age: 26
+}
 
-let colors = ['red', 'green', 'blue']
+console.log(Object.entries(person));
+console.log(Object.values(person));
 
-console.log(colors.includes('yellow'));
+ */
+
+/* async function hi() {
+
+    return 1
+}
+
+hi().then(alert) */
+
+/* async function f() {
+
+    let promise = new Promise((resolve, reject) => {
+        setTimeout(() => resolve("done!"), 1000)
+    });
+
+    let result = await promise; // wait until the promise resolves (*)
+
+    alert(result); // "done!"
+}
+
+f();
+ */
+
+async function myDisplay() {
+    let myPromise = new Promise(function(myResolve, myReject) {
+        setTimeout(function() { myResolve("I love You !!"); }, 3000);
+    });
+    document.getElementById("demo").innerHTML = await myPromise;
+}
+
+myDisplay();
